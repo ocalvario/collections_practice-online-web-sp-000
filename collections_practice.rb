e5527dc08c6103b3987c6c0d7a2d87385d727779
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-19 22:28:21 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+   array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+  
+ reverse_array(array)
+   array.reverse do |a, b|
+    b <=> a
+  end
+end
+
+end
